d72a6bc24dd9fb7cd31b9e88a3f780fb5450ed2b
git-zahidul-islam <freelancerzahidf101@gmail.com>
2024-04-21 09:33:35 +0600

    crud clients complete

--- src/components/Update.jsx
+++ src/components/Update.jsx
@@ -1,8 +1,42 @@
+import { useLoaderData } from "react-router-dom";
+import Nav from "./Nav";
 
 const Update = () => {
+    const loadingUser = useLoaderData()
+    const handleUpdate = e => {
+        e.preventDefault()
+        const form = e.target;
+        const name = form.name.value;
+        const email = form.email.value;
+        console.log(name,email);
+        const updatedUser = {name,email}
+
+        fetch(`http://localhost:5000/users/${loadingUser._id}`,{
+            method: 'PUT',
+            headers: {
+                'content-type' : 'application/json'
+            },
+            body: JSON.stringify(updatedUser)
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            if(data.modifiedCount>0){
+                alert("user update successfully")
+            }
+        })
+    }
     return (
         <div>
-                        
+            <Nav></Nav>
+            <h1>updated data: {loadingUser.name}</h1>
+            <form onSubmit={handleUpdate}>
+                <input type="text" name="name" defaultValue={loadingUser.name} id="" />
+                <br />
+                <input type="email" name="email" defaultValue={loadingUser.email} id="" />
+                <br />
+                <input type="submit" value="Update data" />
+            </form>
         </div>
     );
 };
